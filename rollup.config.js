b90245ad5355012a0cdef380af03183be4fd4f95
James Barzegar <james.barzegar@gmail.com>
2018-09-18 00:49:40 -0400

    WIP

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
-import fileSize from 'rollup-plugin-filesize'
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
+import fileSize from "rollup-plugin-filesize";
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
 
-const plugins = [fileSize(), babel(), resolve()]
+const plugins = [fileSize(), babel(), resolve()];
 
-const constructBuildFiles = (filename = 'index') => ({
+const constructBuildFiles = (filename = "index") => ({
   input: `lib/${filename}.js`,
   output: {
     file: `dist/${filename}.js`,
-    format: 'es'
+    format: "es"
   },
-  external: ['react', 'storybook-readme'],
+  external: ["react", "@storybook/react", "storybook-readme"],
   plugins
-})
+});
 
-export default [constructBuildFiles()]
+export default [constructBuildFiles()];
